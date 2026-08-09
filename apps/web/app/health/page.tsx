e3f11db7a7528ee9bb8e7cf1f3602dc953bf63f1
Amit Dewangan <dewanganamit40@gmail.com>
2026-08-09 14:24:18 +0530

    feature update in mobile interface

--- apps/web/app/health/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import {trpc} from "../../trpc/trpc"
-export default function Health() {
-    const healthQuery = trpc.health.useQuery();
-    console.log("healthQuery", healthQuery)
-    return <h1>Health Check</h1>
-}
-
